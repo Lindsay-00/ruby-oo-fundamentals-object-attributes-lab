102148e4a85474c80fdedbd60b5d516f12b5adeb
Lindsay <lindsay.chen@yale.edu>
2021-06-10 21:09:56 +0800

    object attributes

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+    
+    def initialize 
+        @name 
+        @breed
+    end
+
+    def name 
+        @name = "Fido"
+    end
+
+    def breed 
+        @breed = "Beagle"
+    end
+
+    def name= (new_name)
+        @name = new_name
+    end
+
+    def breed= (new_breed)
+        @breed = new_breed
+    end
+end
